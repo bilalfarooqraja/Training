576694a21d2a7e4502bffaaa04a5c68d54a96835
bilalfarooqraja <34212334+bilalfarooqraja@users.noreply.github.com>
2022-10-19 06:15:31 +0500

    Plus Final
    Plus Final

--- Main.java
+++ Main.java
@@ -1,28 +1,61 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
 
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println("Array" + Arrays.toString(array));
-        arraySort(array);
-        System.out.println("Array" + Arrays.toString(array));
+        System.out.print("Enter number ");
+
+        // takes input from the keyboard
+        int num = Integer.parseInt(input.nextLine());
+
+        System.out.print("Enter array "+num+ " Times");
+
+        int array[]= new int[num];
+        int ar[]=arrayInput(num,array);
+        int arr[]=posNeg(ar);
+        takingAverage(arr,ar);
+
     }
 
-    public static void arraySort(int[] array) {
-        int max = array.length - 1;
-        // System.out.println("max"+max);
-        int halflength = array.length / 2;
-        //System.out.println("half"+halflength);
+    public static int[] arrayInput(int num,int arr[]){
 
-        for (int i = 0; i < halflength; i++) {
-            int temp = array[i];
+        for(int i=0;i<num;i++){
+            Scanner input = new Scanner(System.in);
 
-            array[i] = array[max];
-            array[max] = temp;
-            max--;
+            arr[i] = Integer.parseInt(input.nextLine());
         }
+        return arr;
+    }
+    public static int[] posNeg(int arr[]){
+
+        int P=0,N = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                N++;
+            } else if (arr[i]>=0) {
+                P++;
+            }
+
+        }
+        System.out.println("Postive numbers are "+P+"\nNegative numbers are "+N);
+        int ar[]= new int[2];
+        ar[0]=N;
+        ar[1]=P;
+
+       return ar;
+    }
+    public static void takingAverage(int ar[],int array[]){
+
+        int N=ar[0];
+        int P=ar[1];
+        double Ne=((double) N/array.length);
+        double Po=((double) P/array.length);
+        System.out.println(Ne+"\n"+Po);
+//        long a = 3004230L; // Use the L notation to declare this literal a long.
+//        long b = 6793368L;
+//        double c = ((double) a/b)*100;
 
     }
 }
